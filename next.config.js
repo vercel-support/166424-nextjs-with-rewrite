31eb779a40329e13b83ce92c37c6161209f6cd38
William Li <email@williamli.dev>
2023-11-07 11:24:51 +0800

    added rewrites to /demo-blog

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/dream-blog',
+        destination:
+          'https://166424-gatsbyjs-with-path-prefix.preview.vercel-support.app/demo-blog',
+      },
+      {
+        source: '/dream-blog/:path*',
+        destination:
+          'https://166424-gatsbyjs-with-path-prefix.preview.vercel-support.app/demo-blog/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
